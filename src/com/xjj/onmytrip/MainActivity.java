24362653fb5c85f5ead0d802d3e41e681ea5706f
xujijun <xujijun@126.com>
2013-11-09 16:10:29 +0800

    Modified First version

--- src/com/xjj/onmytrip/MainActivity.java
+++ src/com/xjj/onmytrip/MainActivity.java
@@ -1,85 +1,44 @@
 package com.xjj.onmytrip;
 
-import java.util.Locale;
-
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+public class MainActivity extends FragmentActivity{
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
-
+	TextView textViewCurrentTripInfo;
+	Button buttonSearchHistory;
+	Button buttonGoToMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Set up the action bar.
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		textViewCurrentTripInfo = (TextView) findViewById(R.id.textViewCurrentTripInfo);
+		
+		buttonSearchHistory = (Button) findViewById(R.id.buttonSearchHistory);
+		buttonSearchHistory.setOnClickListener(new OnClickListener() { //查询历史
+			@Override
+			public void onClick(View v) {
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+			}
+		});
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
-		}
+		buttonGoToMap = (Button) findViewById(R.id.buttonGoToMap);
+		buttonGoToMap.setOnClickListener(new OnClickListener() { //打开地图
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, MapActivity.class));
+			}
+		});
+		
 	}
 
 	@Override
@@ -89,140 +48,4 @@ public class MainActivity extends FragmentActivity implements
 		return true;
 	}
 
-	@Override
-	public void onTabSelected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-		// When the given tab is selected, switch to the corresponding page in
-		// the ViewPager.
-		mViewPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a MainFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment=null;
-			
-			switch (position) {
-			case 0:
-				fragment = new MainFragment();
-				Bundle args = new Bundle();
-				args.putInt(MainFragment.ARG_SECTION_NUMBER, position + 1);
-				fragment.setArguments(args);
-				break;
-			case 1:
-				fragment = new SearchFragment();
-				break;
-			case 2:
-				fragment = new SearchFragment();
-				break;
-			}
-			
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * Main fragment to display the map.
-	 */
-	public static class MainFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public MainFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main,
-					container, false);
-			//TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-			//dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-
-	/**
-	 * Search fragment to search the history.
-	 */
-	public class SearchFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-
-		private GoogleMap mMap;
-		
-		public SearchFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_search_history,container, false);
-			setUpMapIfNeeded();
-			return rootView;
-		}
-		
-	    private void setUpMapIfNeeded() {
-	        // Do a null check to confirm that we have not already instantiated the map.
-	        if (mMap == null) {
-	            // Try to obtain the map from the SupportMapFragment.
-	            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-	            // Check if we were successful in obtaining the map.
-	            if (mMap != null) {
-	                setUpMap();
-	            }
-	        }
-	    }
-	    
-	    private void setUpMap() {
-	        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-	    }
-	}
 }
